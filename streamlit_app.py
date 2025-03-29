8ec705da166182dc948c7d4fca8f08e180d1490a
Simon Liang <87677442+SimonYYL@users.noreply.github.com>
2025-03-29 18:49:47 +0000

    template

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+with open("dashboard.html", "r", encoding="utf-8") as f:
+    html_string = f.read()
+
+components.html(
+    html_string,
+    height=1000,  # You can increase this if needed, or make it responsive below
+    width=2000
 )
